d65a17e79f519d55904ad0ed90b9afd027ae2721
David Figueroa <dmfigueroa@uninorte.edu.co>
2020-10-30 04:58:29 +0000

    Theme added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -14,5 +15,17 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.user = this.auth.getLoggedUser();
+    (() => {
+      'use strict';
+      // ENABLE sidebar menu tabs
+      $('.js-sidebar-mini-tabs [data-toggle="tab"]').on('click', function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+      });
+      $('.js-sidebar-mini-tabs').on('show.bs.tab', (e) => {
+          $('.js-sidebar-mini-tabs > .active').removeClass('active');
+          $(e.target).parent().addClass('active');
+        });
+    })();
   }
 }
