bcb7909e466f0bcce2ccc24c32214afe72d8e7c8
root <root@vmi364739.contaboserver.net>
2020-11-13 14:50:31 +0100

    Update to Angular 11

--- src/app/components/shared/right-info/right-info.component.spec.ts
+++ src/app/components/shared/right-info/right-info.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RightInfoComponent } from './right-info.component';
 
@@ -6,7 +6,7 @@ describe('RightInfoComponent', () => {
   let component: RightInfoComponent;
   let fixture: ComponentFixture<RightInfoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RightInfoComponent ]
     })
