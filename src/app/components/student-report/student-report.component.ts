2820293c63937234548fe7cc6070415df83f87db
Martin Molinares <memolinares@uninorte.edu.co>
2020-11-16 22:44:22 -0500

    feat: student report char config

--- src/app/components/student-report/student-report.component.ts
+++ src/app/components/student-report/student-report.component.ts
@@ -1,85 +1,80 @@
 import { Observable } from 'rxjs';
 import { ForumsService } from './../../services/forums.service';
-import { AfterViewInit, Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { firestore } from 'firebase';
 import { User } from 'src/app/models/user.model';
 import { Attempt } from 'src/app/models/attempts';
-import { Chart } from 'chart.js';
+import { Chart } from 'node_modules/chart.js';
 import { ElementRef } from '@angular/core';
-import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-student-report',
   templateUrl: './student-report.component.html',
   styleUrls: ['./student-report.component.scss']
 })
-export class StudentReportComponent implements OnInit, AfterViewInit {
+export class StudentReportComponent implements OnInit, AfterContentInit {
 
+  @ViewChild('test') canvasRef: ElementRef;
 
-  @ViewChildren('canvas') public canvas: QueryList<ElementRef>;
   cid: string;
   eid: string;
   student: Observable<User>;
+  ctx;
 
-  private intervalUpdate: any = null;
-  public chart: any = null;
-  private canvasCom: ElementRef;
-
-  constructor(private route: ActivatedRoute, private forumsService: ForumsService, private elementRef: ElementRef) { }
+  constructor(
+    private route: ActivatedRoute,
+    private forumsService: ForumsService,
+    private elementRef: ElementRef
+    ) { }
 
   ngOnInit(): void {
-    let htmlRef = this.elementRef.nativeElement.querySelector(`#canvas`);
-    var chart = new Chart(htmlRef, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  }
-
-  ngAfterViewInit() {
     this.eid = this.route.snapshot.paramMap.get('eid');
     this.cid = this.route.snapshot.paramMap.get('cid');
     this.student = this.forumsService.getUserInfo(this.eid);
-    // this.intervalUpdate = setInterval(function(){
-    //   this.showData();
-    // }.bind(this), 500);
-
   }
 
-  private ngOnDestroy(): void {
-    clearInterval(this.intervalUpdate);
+  ngAfterContentInit() {
+    setTimeout(() => {
+      console.log(this.canvasRef.nativeElement)
+      this.ctx = this.canvasRef.nativeElement.getContext('2d');
+      var chart = new Chart(this.ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      });
+    }, 100);
   }
 
   getNotaMx(intentos: Array<Attempt>): number {
